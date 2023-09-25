6a5d2d77c62a7a8ef518f0c7f4ac79d43519efb9
konrad-synergy <95217082+konrad-synergy@users.noreply.github.com>
2023-09-25 16:49:21 +0200

    Initial commit

---
+++ apps/app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ai-suple-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'app';
+}
